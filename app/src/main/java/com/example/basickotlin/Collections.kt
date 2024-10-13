33a934550d9f8efbd3424e250af8728c75e625ad
BriliyanluthfiTriandri <briliyanlt@gmail.com>
2024-10-13 21:45:51 +0700

    membuat fungsi ContohMap

--- app/src/main/java/com/example/basickotlin/Collections.kt
+++ app/src/main/java/com/example/basickotlin/Collections.kt
@@ -63,3 +63,39 @@ fun ContohSet(){
     val shapeLocked: Set<String> = shape
     println(shapeLocked)
 }
+
+// Map adalah kumpulan data yang disusuan dalma pasangan key-value
+// Map bersifat unordered, artinya data yang dimasukan tidak memiiki posisi tertentu
+// Map bersifat mutable dan read only, artinya data yang dimasukan dapat diubah dan tidak dapat diubah
+// Map Read-Only mengguanakn mapOf
+// Map Mutable menggunakan mutableMapOf
+
+fun ContohMap(){
+
+    println()
+    println("=== Map ===")
+    // Map Read-Only
+    val readOnlyShape: Map<String, Int> = mapOf("Circle" to 1, "Square" to 2, "Triangle" to 3)
+    println(readOnlyShape)
+
+    // Map Mutable
+    val shape: MutableMap<String, Int> = mutableMapOf("Circle" to 1, "Square" to 2, "Triangle" to 3)
+    println(shape)
+
+    // Menambahkan data ke dalam Map Mutable
+    shape["Rectangle"] = 4
+    println(shape)
+
+    // Menghapus data dari Map Mutable
+    shape.remove("Circle")
+    println(shape)
+
+    // Mengubah data di dalam Map Mutable
+    shape["Square"] = 5
+    println(shape)
+
+    // Map Read-Only
+    val shapeLocked: Map<String, Int> = shape
+    println(shapeLocked)
+}
+
