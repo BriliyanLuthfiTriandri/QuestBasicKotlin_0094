276cb792225b3dff40f587e8b277b4e4a35e3d63
BriliyanluthfiTriandri <briliyanlt@gmail.com>
2024-10-13 21:09:34 +0700

    membuat functional ConditionalStatement dengan kondisi try-catch

--- app/src/main/java/com/example/basickotlin/ConditionalExpression.kt
+++ app/src/main/java/com/example/basickotlin/ConditionalExpression.kt
@@ -27,4 +27,12 @@ fun ConditionalStatement() {
         else -> println("Invalid Day")
     }
 
+    //Try-Catch
+    val value = "10"
+    try {
+        val result = value.toInt()
+        println("Result: $result")
+    } catch (e: NumberFormatException) {
+        println("Invalid Number")
+    }
 }
